62f036a98c5b4a4c8aa8fbefdfc3e1ec07b8c5b0
Neverslave <1029110566@qq.com>
2019-01-02 22:41:02 +0800

    666

--- src/main/webapp/admin/js/index.js
+++ src/main/webapp/admin/js/index.js
@@ -1,6 +1,6 @@
 var $,tab,skyconsWeather;
 layui.config({
-	base : "js/"
+	base : "/admin/js/"    //扩展模块的根目录
 }).use(['bodyTab','form','element','layer','jquery'],function(){
 	var form = layui.form(),
 		layer = layui.layer,
